2d17dcf61150ef819ba2e1b20abf95196d76e34f
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-10-04 21:09:17 +0330

    "then" and "catch" removed from api function.

--- src/api/siteApi.js
+++ src/api/siteApi.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { handleResponse, handleError } from "./apiUtils";
 
 const url = "efs";
 
 export function getSites() {
-  return axios.get(url).then(handleResponse).catch(handleError);
+  return axios.get(url);
 }
