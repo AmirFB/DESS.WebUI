934a8123cc551b04bf0a4df51288b495b98f5372
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-10-04 21:14:19 +0330

    Representative component "SiteGrid" added and redux problems solved.

--- src/components/main/body/site/siteList.jsx
+++ src/components/main/body/site/siteList.jsx
@@ -1,53 +1,30 @@
 import React from "react";
-import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 import "@progress/kendo-theme-material/dist/all.css";
 import { connect } from "react-redux";
-import * as siteActions from "../../../../redux/actions/siteActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import SiteGrid from "./siteGrid";
+import * as siteActions from "../../../../redux/actions/siteActions";
 
-export class SiteList extends React.Component {
+class SiteList extends React.Component {
   componentDidMount() {
-    const { sites, actions } = this.props;
+    const { siteRecuder, actions } = this.props;
+    const sites = siteRecuder.sites;
 
-    actions.getSites();
+    if (sites.length === 0) {
+      actions.getSites().catch((error) => {
+        alert("Loading sites failed" + error);
+      });
+    }
+
+    setTimeout(() => {
+      console.log("sites");
+      console.log(this.props.siteRecuder.sites);
+    }, 3000);
   }
 
   render() {
-    return (
-      <div>
-        <Grid style={{ height: "100%" }} data={this.props.sites}>
-          <Column field="id" title="ID" width="40px" />
-          <Column field="name" title="Name" width="90px" />
-          <Column field="siteId" title="Site ID" width="120px" />
-          <Column field="serialNo" title="Serial Number" width="120px" />
-          <Column field="phoneNumber" title="Phone Number" width="120px" />
-          <Column field="ipAddress" title="IP Address" width="120px" />
-          <Column
-            field="useGlobalIntervarl"
-            title="Use global interval"
-            width="150px"
-          />
-          <Column field="interval" title="Interval" width="80px" />
-          <Column field="autoLocation" title="Auto Location" width="90px" />
-          <Column field="latitude" title="Latitude" width="110px" />
-          <Column field="longitude" title="Longitude" width="110px" />
-          <Column field="hvEnabled" title="HV enabled" width="100px" />
-          <Column field="lvEnabled" title="LV enabled" width="100px" />
-          <Column field="hvPower" title="HV power" />
-          <Column field="hvThreshold" title="HV treshold" />
-          <Column field="hvRepeat" title="HV repeat" />
-          <Column field="temperatureMin" title="Temperature min" />
-          <Column field="temperatureMax" title="Temperature max" />
-          <Column field="batteryMin" title="Battery min" />
-          <Column field="batteryMax" title="Battery max" />
-          <Column field="input1" title="Input 1" />
-          <Column field="input2" title="Input 2" />
-          <Column field="output1" title="Output 1" />
-          <Column field="output2" title="Output 2" />
-        </Grid>
-      </div>
-    );
+    return <SiteGrid sites={this.props.siteRecuder.sites} />;
   }
 }
 
@@ -58,7 +35,7 @@ SiteList.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    sites: state.sites,
+    siteRecuder: state.siteRecuder,
   };
 }
 
