bbccb626e25bb8398a6568c6e777f792120e5fab
AlirezaFB <alirezaf.babai@gmail.com>
2020-10-05 14:31:45 +0330

    Tabs Added in "Body" component.

--- src/components/main/body/body.jsx
+++ src/components/main/body/body.jsx
@@ -5,6 +5,10 @@ import "@progress/kendo-theme-material/dist/all.css";
 
 import Home from "./home/home";
 import Sidebar from "../sideBar/sideBar";
+import Settings from "./settings/settings";
+import Users from "./user/userList";
+import Report from "./report/report";
+import About from "./about";
 
 import "./body.css";
 import SiteList from "./site/siteList";
@@ -34,31 +38,21 @@ export default function Body() {
             <SiteList></SiteList>
           </TabStripTab>
 
-          <TabStripTab title="Tallinn">
-            <div>
-              <p>
-                Tallinn is the capital and largest city of Estonia. It is
-                situated on the northern coast of the country, on the shore of
-                the Gulf of Finland, 80 km (50 mi) south of Helsinki, east of
-                Stockholm and west of Saint Petersburg. From the 13th century
-                until 1918 (and briefly during the Nazi occupation of Estonia
-                from 1941 to 1944), the city was known as Reval. Tallinn
-                occupies an area of 159.2 km2 (61.5 sq mi) and has a population
-                of 443,894. Approximately 32% of Estonia's total population
-                lives in Tallinn.
-              </p>
-              <p>
-                Tallinn was founded in 1248, but the earliest human settlements
-                are over 5,000 years old, making it one of the oldest capital
-                cities of Northern Europe. Due to its strategic location, the
-                city became a major trade hub, especially from the 14th to the
-                16th century, when it grew in importance as part of the
-                Hanseatic League.
-              </p>
-            </div>
+          <TabStripTab title="Settings">
+            <Settings />
           </TabStripTab>
 
-          <TabStripTab title="Sydney">jfasjfisaj</TabStripTab>
+          <TabStripTab title="Users">
+            <Users />
+          </TabStripTab>
+
+          <TabStripTab title="Report">
+            <Report />
+          </TabStripTab>
+
+          <TabStripTab title="About">
+            <About />
+          </TabStripTab>
         </TabStrip>
       </div>
     </div>
