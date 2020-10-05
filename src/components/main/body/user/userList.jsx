b072dfdce883d22c136ae4538f01087289427aa3
AlirezaFB <alirezaf.babai@gmail.com>
2020-10-05 14:20:03 +0330

    Implemented "UserList" component.

---
+++ src/components/main/body/user/userList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "@progress/kendo-theme-material/dist/all.css";
+import Users from "./users.json";
+
+export default function UsersList() {
+  return (
+    <div>
+      <div>Icons place</div>
+
+      <Grid style={{ height: "100%" }} data={[...Users]}>
+        <Column field="ID" title="ID" width="40px" />
+        <Column field="Username" title="Username" />
+        <Column field="Firstname" title="Firstname" />
+        <Column field="Lastname" title="Lastname" />
+        <Column field="Group" title="Group" />
+      </Grid>
+    </div>
+  );
+}
