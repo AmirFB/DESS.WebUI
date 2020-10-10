08d0ca5ea18bb6177f9628f0ce7222d36b98ab00
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-10-10 21:32:02 +0330

    "apiStatusReducer" implemented.

---
+++ src/redux/reducers/apiStatusReducer.js
@@ -0,0 +1,17 @@
+import { BEGIN_API_CALL, API_CALL_ERROR } from "../actions/actionTypes";
+import { isSuccess } from "../helpers/helpers";
+
+const initialState = { apiCallsInProgress: 0 };
+
+export default function apiStatusReducer(
+  state = initialState.apiCallsInProgress,
+  action
+) {
+  if (action.type == BEGIN_API_CALL) {
+    return state + 1;
+  } else if (action.type === API_CALL_ERROR || isSuccess(action.type)) {
+    return state - 1;
+  }
+
+  return state;
+}
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-import siteReducer from "./siteReducer";
 
-const rootReducer = combineReducers({ siteReducer });
+import siteReducer from "./siteReducer";
+import apiStatusReducer from "./apiStatusReducer";
+
+const rootReducer = combineReducers({ siteReducer, apiStatusReducer });
 
 export default rootReducer;
