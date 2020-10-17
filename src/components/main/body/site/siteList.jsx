01fd67bb62182f6fc925f44132dadd08e6d0075f
AlirezaFB <alirezaf.babai@gmail.com>
2020-10-17 15:13:47 +0330

    Added "siteConfig".

--- src/components/main/body/site/siteList.jsx
+++ src/components/main/body/site/siteList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import SiteGrid from "./siteGrid";
+import SiteConfig from "./siteConfig";
 
 import { connect } from "react-redux";
 import { useTranslation } from "react-i18next";
@@ -10,6 +11,8 @@ import {
 } from "@progress/kendo-react-notification";
 import { Zoom } from "@progress/kendo-react-animation";
 import Loading from "../../../common/loading";
+import { Button } from "@progress/kendo-react-buttons";
+import { Route, Redirect } from "react-router-dom";
 
 import * as siteActions from "../../../../redux/actions/siteActions";
 
@@ -18,6 +21,8 @@ import "@progress/kendo-theme-material/dist/all.css";
 function SiteList({ siteReducer, getSites, ...props }) {
   const [t, i18n] = useTranslation();
   const [getFailed, setGetFailed] = useState(false);
+
+  const [redirect, setRedirect] = useState(false);
 
   useEffect(() => {
     getSites().catch((error) => {
@@ -31,37 +36,53 @@ function SiteList({ siteReducer, getSites, ...props }) {
     });
   }, [props.sites]);
 
+  function handleAddSite() {
+    setRedirect(true);
+  }
+
   return (
     <div className="list-div">
-      <h1>{t("common.sites")}</h1>
-      {siteReducer.loading ? (
-        <Loading />
-      ) : siteReducer.hasError && siteReducer.sites.length === 0 ? (
-        <h3>{t("common.getFailed")}</h3>
+      {redirect ? (
+        <>
+          <Redirect push to="/siteList/siteEdit" />
+          <Route path="/siteList/siteEdit" component={SiteConfig} />
+        </>
       ) : (
-        <SiteGrid sites={siteReducer.sites} />
-      )}
+        <>
+          <Button onClick={handleAddSite} primary>
+            {t("site.addSite")}
+          </Button>
 
-      <NotificationGroup
-        style={{
-          right: 0,
-          bottom: 0,
-          alignItems: "flex-start",
-          flexWrap: "wrap-reverse",
-        }}
-      >
-        <Zoom enter={true} exit={true}>
-          {getFailed && (
-            <Notification
-              type={{ style: "error", icon: true }}
-              closable={true}
-              onClose={() => this.setGetFailed(false)}
-            >
-              <span>{t("site.getSitesFailed")}</span>
-            </Notification>
+          {siteReducer.loading ? (
+            <Loading />
+          ) : siteReducer.hasError && siteReducer.sites.length === 0 ? (
+            <h3>{t("common.getFailed")}</h3>
+          ) : (
+            <SiteGrid sites={siteReducer.sites} />
           )}
-        </Zoom>
-      </NotificationGroup>
+
+          <NotificationGroup
+            style={{
+              right: 0,
+              bottom: 0,
+              alignItems: "flex-start",
+              flexWrap: "wrap-reverse",
+            }}
+          >
+            <Zoom enter={true} exit={true}>
+              {getFailed && (
+                <Notification
+                  type={{ style: "error", icon: true }}
+                  closable={true}
+                  onClose={() => this.setGetFailed(false)}
+                >
+                  <span>{t("site.getSitesFailed")}</span>
+                </Notification>
+              )}
+            </Zoom>
+          </NotificationGroup>
+        </>
+      )}
     </div>
   );
 }
