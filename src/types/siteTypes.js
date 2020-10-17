72e598fefada4597842a524cdab85000475c9d0d
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-10-17 20:59:49 +0330

    "siteTypes" implemented.

---
+++ src/types/siteTypes.js
@@ -0,0 +1,17 @@
+export const statusType = {
+  Clear: 0,
+  Warning: 1,
+  Fault: 2,
+};
+
+export const ioType = {
+  NO: 0,
+  NC: 1,
+};
+
+export const batteryStatusType = {
+  Charging: 0,
+  Charged: 1,
+  Low: 2,
+  Fault: 3,
+};
