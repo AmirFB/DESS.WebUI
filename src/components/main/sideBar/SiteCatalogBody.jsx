e63564e316c7c2c49b26b3e96be2c3db6603b2d6
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-10-24 18:12:14 +0330

    Side bar structure updated.

--- src/components/main/sideBar/SiteCatalogBody.jsx
+++ src/components/main/sideBar/SiteCatalogBody.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Divider from "@material-ui/core/Divider";
+import blue from "@material-ui/core/colors/blue";
 
-import { Container, Row, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,61 +18,123 @@ export default function SiteCatalogBody({ site, ...props }) {
     );
 
   return (
-    <Container className="site-catalog-body">
-      <Row>
-        <Col>
+    <Grid
+      container
+      direction="column"
+      className="site-catalog-body"
+      style={{
+        backgroundColor: blue[100],
+      }}
+    >
+      <Grid
+        container
+        item
+        direction="row"
+        justify="space-between"
+        alignItems="baseline"
+      >
+        <Grid item lg>
           HV:&nbsp;
           {statusText(site.status.hvAlarm)}
-        </Col>
-        <Col>
+        </Grid>
+        <Grid item lg>
           LV:&nbsp;
           {statusText(site.status.lvAlarm)}
-        </Col>
-        <Col>
+        </Grid>
+        <Grid item lg>
           Tamper:&nbsp;
           {statusText(site.status.tamperAlarm)}
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid
+        container
+        item
+        direction="row"
+        justify="space-between"
+        alignItems="baseline"
+      >
+        <Grid item lg>
           {t("catalog.power")}:&nbsp;
           <span style={{ color: "blue" }}>{site.hvPower}%</span>
-        </Col>
-        <Col>
+        </Grid>
+        <Grid item lg>
           {t("catalog.voltage")}:&nbsp;
           <span style={{ color: "blue" }}>{site.status.hvVoltage}</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        item
+        direction="row"
+        justify="space-between"
+        alignItems="baseline"
+      >
+        <Grid item lg>
           {t("catalog.mainPower")}:&nbsp;
           {statusText(site.status.mainPowerFault)}
-        </Col>
-        <Col>
+        </Grid>
+        <Grid item lg>
           {t("catalog.hvCharge")}:&nbsp;{statusText(site.status.hvChargeFault)}
-        </Col>
-      </Row>
-      <Row>
-        <Col>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        item
+        direction="row"
+        justify="space-between"
+        alignItems="baseline"
+      >
+        <Grid item lg>
           {t("catalog.hvPower")}:&nbsp;{statusText(site.status.hvPowerFault)}
-        </Col>
-        <Col>
+        </Grid>
+        <Grid item lg>
           {t("catalog.hvDischarge")}:&nbsp;
           {statusText(site.status.hvDischargeFault)}
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col>
-          {t("catalog.power")}:&nbsp;
-          <span style={{ color: "blue" }}>{site.hvPower}%</span>
-        </Col>
-        <Col>
-          {t("catalog.voltage")}:&nbsp;
-          <span style={{ color: "blue" }}>{site.status.hvVoltage}</span>
-        </Col>
-      </Row>
-    </Container>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid
+        container
+        direction="column"
+        justify="space-between"
+        alignItems="baseline"
+      >
+        <Grid
+          container
+          item
+          direction="row"
+          justify="space-between"
+          alignItems="baseline"
+        >
+          <Grid item lg>
+            {t("catalog.temperature")}:&nbsp;
+            <span
+              style={{
+                color:
+                  site.status.temperature > site.temperatureMax
+                    ? "red"
+                    : site.status.temperature < site.temperatureMin
+                    ? "cyan"
+                    : "blue",
+              }}
+            >
+              {site.status.temperature}
+            </span>
+          </Grid>
+          <Grid item lg>
+            {t("catalog.battery")}:&nbsp;
+            <span
+              style={{
+                color:
+                  site.status.batteryLevel < site.batteryMin ? "red" : "ble",
+              }}
+            >
+              {site.status.batteryLevel}
+            </span>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
