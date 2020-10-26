81f5d3b7ec0c96928c1296f0a8879e8aea0c07f0
AlirezaFB <alirezaf.babai@gmail.com>
2020-10-26 17:36:16 +0330

    Edited "reportGrid".

--- src/components/main/body/report/reportGrid.jsx
+++ src/components/main/body/report/reportGrid.jsx
@@ -1,52 +1,43 @@
 import React, { useEffect, useState } from "react";
 import PropType from "prop-types";
 import { DataGrid } from "@material-ui/data-grid";
+import { dateToString } from "../../../../helpers/dateTime";
 
 import { useTranslation } from "react-i18next";
 
 export default function ReportGrid(props) {
   const [t, i18n] = useTranslation();
   const [rows, setRows] = useState([]);
+
   const columns = [
-    { field: "applied", headerName: t("report.applied"), width: 100 },
-    { field: "ipAddress", headerName: t("common.ipAdd"), width: 130 },
-    { field: "serialNo", headerName: t("site.serialNo"), width: 90 },
-    { field: "hvAlarm", headerName: t("report.hvAlarm"), width: 93 },
-    { field: "lvAlarm", headerName: t("report.lvAlarm"), width: 90 },
-    { field: "tamperAlarm", headerName: t("report.tamperAlarm"), width: 121 },
+    { field: "seenBy", headerName: t("report.seenBy"), width: 200 },
     {
-      field: "mainPowerFault",
-      headerName: t("report.mainPowerFault"),
-      width: 146,
-    },
-    { field: "hvPowerFault", headerName: t("report.hvPowerFault"), width: 133 },
-    {
-      field: "hvChargeFault",
-      headerName: t("report.hvChargeFault"),
-      width: 139,
+      field: "occuredOn",
+      headerName: t("report.occuredOn"),
+      width: 135,
+      renderCell: (params) =>
+        params.getValue("occuredOn") > 0
+          ? dateToString(params.getValue("occuredOn"))
+          : "-",
     },
     {
-      field: "hvDischargeFault",
-      headerName: t("report.hvDichargeFault"),
-      width: 157,
+      field: "resetedOn",
+      headerName: t("report.resetedOn"),
+      width: 135,
+      renderCell: (params) =>
+        params.getValue("resetedOn") > 0
+          ? dateToString(params.getValue("resetedOn"))
+          : "-",
     },
-    { field: "temperature", headerName: t("common.temperature"), width: 113 },
+    { field: "resetedBy", headerName: t("report.resetedBy"), width: 200 },
     {
-      field: "batteryStatus",
-      headerName: t("report.batteryStatus"),
-      width: 123,
-    },
-    { field: "batteryLevel", headerName: t("report.batteryLevel"), width: 116 },
-    { field: "input1", headerName: t("common.input1"), width: 90 },
-    { field: "input2", headerName: t("common.input2"), width: 90 },
-    { field: "output1", headerName: t("common.output1"), width: 90 },
-    { field: "output2", headerName: t("common.output2"), width: 90 },
-    { field: "latitude", headerName: t("common.latitude"), width: 110 },
-    { field: "longitude", headerName: t("common.longitude"), width: 120 },
-    {
-      field: "signalStrength",
-      headerName: t("report.signalStrength"),
-      width: 130,
+      field: "obviatedOn",
+      headerName: t("report.obviatedOn"),
+      width: 135,
+      renderCell: (params) =>
+        params.getValue("obviatedOn") > 0
+          ? dateToString(params.getValue("obviatedOn"))
+          : "-",
     },
   ];
 
