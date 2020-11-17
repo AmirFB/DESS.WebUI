602607e4c086be2ab94491af0a1732e2a2617630
AlirezaFB <alirezaf.babai@gmail.com>
2020-11-17 17:17:09 +0330

    Added "SiteGroup" components.

---
+++ src/components/main/body/site/siteGroupGrid.jsx
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+
+import { DataGrid } from "@material-ui/data-grid";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import EditIcon from "@material-ui/icons/Edit";
+import red from "@material-ui/core/colors/red";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+function SiteGroupGrid({ groups, permissions, removeGroup, ...props }) {
+  const [t, i18n] = useTranslation();
+  const [rows, setRows] = useState([]);
+  const [open, setOpen] = useState(false);
+  const history = useHistory();
+
+  const handleEdit = (id) => {
+    history.push({
+      pathname: "/siteGroups/siteGroupEdit",
+      state: { id },
+    });
+  };
+
+  //console.log(groups);
+
+  const handelRemove = () => {
+    setOpen(true);
+  };
+
+  const handleAccept = (groupId) => {
+    // removeGroup(groupId);
+    // getGroups();
+    setOpen(false);
+  };
+
+  const handleDecline = () => {
+    setOpen(false);
+  };
+
+  const columns = [
+    // {
+    //   field: "title",
+    //   headerName: t("sites.groupName"),
+    //   width: 130,
+    // },
+    // {
+    //   field: "permission",
+    //   headerName: t("sites.permissions"),
+    //   width: 600,
+    //   renderCell: (params) => (
+    //     <>
+    //       {permissions.map((permission) => (
+    //         <>
+    //           <Checkbox
+    //             checked={params
+    //               .getValue("permissionIds")
+    //               .includes(permission.id)}
+    //             color="primary"
+    //           ></Checkbox>
+    //           {t("sites." + permission.title)}
+    //         </>
+    //       ))}
+    //     </>
+    //   ),
+    // },
+    // {
+    //   field: "actions",
+    //   headerName: t("common.actions"),
+    //   width: 125,
+    //   renderCell: (params) => (
+    //     <>
+    //       <IconButton
+    //         aria-label="editGroup"
+    //         size="medium"
+    //         color="primary"
+    //         onClick={() => {
+    //           handleEdit(params.getValue("id"));
+    //         }}
+    //       >
+    //         <EditIcon fontSize="inherit" />
+    //       </IconButton>
+    //       <>
+    //         <IconButton
+    //           aria-label="removeGroup"
+    //           size="medium"
+    //           style={{ color: red[500] }}
+    //           onClick={() => {
+    //             handelRemove(params.getValue("id"));
+    //           }}
+    //         >
+    //           <DeleteForeverIcon fontSize="inherit" />
+    //         </IconButton>
+    //         <Dialog
+    //           open={open}
+    //           onClose={handleDecline}
+    //           aria-labelledby="alert-dialog-title"
+    //           aria-describedby="alert-dialog-description"
+    //         >
+    //           <DialogTitle id="alert-dialog-title">
+    //             {t("dialog.title")}
+    //           </DialogTitle>
+    //           <DialogContent>
+    //             <DialogContentText id="alert-dialog-description">
+    //               {t("dialog.message")}
+    //             </DialogContentText>
+    //           </DialogContent>
+    //           <DialogActions>
+    //             <Button color="primary" onClick={handleDecline}>
+    //               {t("dialog.disagree")}
+    //             </Button>
+    //             <Button
+    //               onClick={() => {
+    //                 handleAccept(params.getValue("id"));
+    //               }}
+    //               color="primary"
+    //               autoFocus
+    //             >
+    //               {t("dialog.agree")}
+    //             </Button>
+    //           </DialogActions>
+    //         </Dialog>
+    //       </>
+    //     </>
+    //   ),
+    // },
+  ];
+
+  useEffect(() => {
+    setRows(groups);
+  }, [groups]);
+
+  return (
+    <div style={{ height: "99%", width: "99%" }}>
+      {/* <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={7}
+        hideFooterSelectedRowCount
+      /> */}
+    </div>
+  );
+}
+
+export default SiteGroupGrid;
---
+++ src/components/main/body/site/siteGroupList.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { useTranslation } from "react-i18next";
+import "@progress/kendo-theme-material/dist/all.css";
+import {
+  Notification,
+  NotificationGroup,
+} from "@progress/kendo-react-notification";
+import { Zoom } from "@progress/kendo-react-animation";
+import Loading from "../../../common/loading";
+import SiteGroupGrid from "./siteGroupGrid";
+import IconButton from "@material-ui/core/IconButton";
+import RefreshIcon from "@material-ui/icons/Refresh";
+
+import * as siteActions from "../../../../redux/actions/siteActions";
+
+function SiteGroupList({ siteReducer, getGroups, getPermissions, ...props }) {
+  const [t, i18n] = useTranslation();
+  const [getFailed, setGetFailed] = useState(false);
+
+  useEffect(() => {
+    if (siteReducer.groups.length === 0)
+      getGroups().catch((error) => {
+        setGetFailed(true);
+
+        if (!getFailed) {
+          setTimeout(() => {
+            setGetFailed(false);
+          }, 10000);
+        }
+      });
+  }, []);
+
+  function handleRefresh() {
+    getGroups().catch((error) => {
+      setGetFailed(true);
+
+      if (!getFailed) {
+        setTimeout(() => {
+          setGetFailed(false);
+        }, 10000);
+      }
+    });
+  }
+
+  return (
+    <>
+      <IconButton
+        aria-label="addGroup"
+        color="primary"
+        size="medium"
+        onClick={handleRefresh}
+      >
+        <RefreshIcon fontSize="inherit" />
+      </IconButton>
+      {siteReducer.loading ? (
+        <Loading />
+      ) : siteReducer.hasError && siteReducer.groups.length === 0 ? (
+        <h3>{t("common.getFailed")}</h3>
+      ) : (
+        <SiteGroupGrid
+          groups={siteReducer.groups}
+          permissions={siteReducer.permissions}
+        />
+      )}
+
+      <NotificationGroup
+        style={{
+          right: 0,
+          bottom: 0,
+          alignItems: "flex-start",
+          flexWrap: "wrap-reverse",
+        }}
+      >
+        <Zoom enter={true} exit={true}>
+          {getFailed && (
+            <Notification
+              type={{ style: "error", icon: true }}
+              closable={true}
+              onClose={() => this.setGetFailed(false)}
+            >
+              <span>{t("site.getSitesFailed")}</span>
+            </Notification>
+          )}
+        </Zoom>
+      </NotificationGroup>
+    </>
+  );
+}
+
+SiteGroupList.propTypes = {
+  siteReducer: PropTypes.object.isRequired,
+  getGroups: PropTypes.func.isRequired,
+  //getPermissions: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    siteReducer: state.siteReducer,
+  };
+}
+
+const mapDispatchToProps = {
+  getGroups: siteActions.getGroups,
+  //getPermissions: siteActions.getPermissions,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SiteGroupList);
