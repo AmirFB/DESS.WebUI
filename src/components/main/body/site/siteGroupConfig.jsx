4bb1c25f308e3240c74710b8ba1758c3e2f61266
AlirezaFB <alirezaf.babai@gmail.com>
2020-12-06 13:09:31 +0330

    Implemented SiteGroupConfig component.

---
+++ src/components/main/body/site/siteGroupConfig.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SiteGroupConfig(props) {
+  return (
+    <div>
+      <div>Site groups's id : {props.location.state.id}</div>
+    </div>
+  );
+}
