2b2809b46729b8a48c9a5b5db324437fef3468bf
AlirezaFB <alirezaf.babai@gmail.com>
2021-01-06 00:31:57 +0330

    Added save button.

--- src/components/main/body/site/siteGroupConfig.jsx
+++ src/components/main/body/site/siteGroupConfig.jsx
@@ -1,51 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import { Label } from "@progress/kendo-react-labels";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 function SiteGroupConfig({ siteReducer, ...props }) {
   const [t, i18n] = useTranslation();
   const groupData = siteReducer.groups.find(
     (id) => id.id == props.location.state.id
   );
+  const history = useHistory();
+  const [open, setOpen] = useState(false);
+  const vertical = "bottom";
+  const horizontal = "right";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push({
+      pathname: "/siteGroups/",
+    });
+  };
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
-    <Grid container direction="column">
-      <Grid container>
-        <Grid item>
-          <TextField
-            name="name"
-            value={groupData.name}
-            label={t("users.groupName")}
-            //onChange={handleChange}
-          />
+    <form onSubmit={handleSubmit}>
+      <Grid container direction="column">
+        <Grid container>
+          <Grid item>
+            <TextField
+              name="name"
+              value={groupData.name}
+              label={t("users.groupName")}
+              //onChange={handleChange}
+            />
+          </Grid>
+          <Grid item style={{ marginLeft: "50px" }}>
+            <TextField
+              name="province"
+              value={groupData.province}
+              label={t("site.provinceName")}
+              //onChange={handleChange}
+            />
+          </Grid>
         </Grid>
-        <Grid item style={{ marginLeft: "50px" }}>
-          <TextField
-            name="province"
-            value={groupData.province}
-            label={t("site.provinceName")}
-            //onChange={handleChange}
-          />
+        <Grid container>
+          <Grid item>
+            <Label>Sites :&nbsp;</Label>
+            <Label>{groupData.siteIds}</Label>
+          </Grid>
+          <Grid item style={{ marginLeft: "50px" }}>
+            {" "}
+            <Label>Users :&nbsp;</Label>
+            <Label>{groupData.userIds}</Label>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item>
+            <Button type="submit" variant="contained" color="primary">
+              {t("common.save")}
+            </Button>
+
+            <Button
+              variant="contained"
+              style={{ margin: "5px 30px" }}
+              onClick={() => {
+                history.push({
+                  pathname: "/siteGroups/",
+                });
+              }}
+            >
+              {t("common.cancel")}
+            </Button>
+            <Snackbar
+              open={open}
+              autoHideDuration={6000}
+              onClose={handleClose}
+              anchorOrigin={{ vertical, horizontal }}
+            >
+              <Alert onClose={handleClose} severity="error">
+                {t("error.saveError")}
+              </Alert>
+            </Snackbar>
+          </Grid>
         </Grid>
       </Grid>
-      <Grid container>
-        <Grid item>
-          <Label>Sites :&nbsp;</Label>
-          <Label>{groupData.siteIds}</Label>
-        </Grid>
-        <Grid item style={{ marginLeft: "50px" }}>
-          {" "}
-          <Label>Users :&nbsp;</Label>
-          <Label>{groupData.userIds}</Label>
-        </Grid>
-      </Grid>
-    </Grid>
+    </form>
   );
 }
 
--- src/components/main/body/user/userGroupConfig.jsx
+++ src/components/main/body/user/userGroupConfig.jsx
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
+
+import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-function UserGroupConfig({ userReducer, ...props }) {
+function UserGroupConfig({ userReducer, siteReducer, getGroups, ...props }) {
   const [t, i18n] = useTranslation();
+  const history = useHistory();
   const [group, setGroup] = useState({
     title: "",
     permissionIds: "",
@@ -23,6 +27,13 @@ function UserGroupConfig({ userReducer, ...props }) {
   const [open, setOpen] = useState(false);
   const vertical = "bottom";
   const horizontal = "right";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push({
+      pathname: "/userGroups/",
+    });
+  };
 
   const handleEdit = (userId, groups) => {};
 
@@ -46,6 +57,8 @@ function UserGroupConfig({ userReducer, ...props }) {
     });
   };
 
+  
+
   const handleClick = () => {
     setOpen(true);
   };
@@ -59,39 +72,59 @@ function UserGroupConfig({ userReducer, ...props }) {
   };
 
   return (
-    <Grid container direction="column">
-      <Grid item>
-        <TextField
-          name="title"
-          value={group.title}
-          label={t("users.groupName")}
-          onChange={handleChange}
-        />
+    <form onSubmit={handleSubmit}>
+      <Grid container direction="column">
+        <Grid item>
+          <TextField
+            name="title"
+            value={group.title}
+            label={t("users.groupName")}
+            onChange={handleChange}
+          />
+        </Grid>
+
+        <Grid container>
+          {userReducer.permissions.map((permission) => (
+            <Grid item>
+              <Checkbox
+                value={permission.id}
+                checked={group.permissionIds.includes(permission.id)}
+                color="primary"
+                onChange={handlePermissionChange}
+              ></Checkbox>
+              {t("users." + permission.title)}
+            </Grid>
+          ))}
+          <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical, horizontal }}
+          >
+            <Alert onClose={handleClose} severity="error">
+              {t("error.saveError")}
+            </Alert>
+          </Snackbar>
+        </Grid>
+        <Grid item>
+          <Button type="submit" variant="contained" color="primary">
+            {t("common.save")}
+          </Button>
+
+          <Button
+            variant="contained"
+            style={{ margin: "5px 30px" }}
+            onClick={() => {
+              history.push({
+                pathname: "/userGroups/",
+              });
+            }}
+          >
+            {t("common.cancel")}
+          </Button>
+        </Grid>
       </Grid>
-      <Grid container>
-        {userReducer.permissions.map((permission) => (
-          <Grid item>
-            <Checkbox
-              value={permission.id}
-              checked={group.permissionIds.includes(permission.id)}
-              color="primary"
-              onChange={handlePermissionChange}
-            ></Checkbox>
-            {t("users." + permission.title)}
-          </Grid>
-        ))}
-        <Snackbar
-          open={open}
-          autoHideDuration={6000}
-          onClose={handleClose}
-          anchorOrigin={{ vertical, horizontal }}
-        >
-          <Alert onClose={handleClose} severity="error">
-            {t("error.saveError")}
-          </Alert>
-        </Snackbar>
-      </Grid>
-    </Grid>
+    </form>
   );
 }
 
